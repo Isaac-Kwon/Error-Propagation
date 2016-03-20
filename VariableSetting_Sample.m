6d7ef3ef8e2722fe0193619110d9dccedf938af8
Isaac Kwon <minals3165@gmail.com>
2016-03-20 23:00:24 +0900

    Initialize : file add

---
+++ VariableSetting_Sample.m
@@ -0,0 +1,43 @@
+clc
+clear
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+Data = ...
+    [     ]; % put the variable data
+
+constantData = {  }; % put the constant data
+
+errorData = [ ]; % put the systemic error
+
+
+
+syms   % put symbolic variable
+
+f =  ; %put symbolic fomula
+
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+filename = input('What will you name the variable file? (.mat) \nIf empty, it will be name ''variablefile.mat''\nFileName : ','s');
+
+if isempty(filename)
+    
+    filename = 'variablefile.mat';
+    
+else
+    
+    filename = [filename '.mat'];
+    
+end
+
+save(filename);
+fprintf('\n \t Save .mat file (name : %s) \n',filename)
+    
---
+++ errorpropagation_calculation.m
@@ -0,0 +1,134 @@
+%Calculation code of Error Propagation // Need Symbolic Math Toolbox
+
+exist filename 
+i = ans
+
+TF=0;
+
+clc
+if (i==1&&ischar(filename))
+   
+    fprintf('Datafile Founded (filename : ''%s'')\n',filename)
+    answer = input('Will you open?\nIf you will, press ENTER, if not press any OTHER key ans press ENTER', 's')
+    
+    
+    if isempty(answer)
+        TF = 1;
+    end
+    
+    
+    
+end
+
+if ~TF
+    
+    clc
+    clear
+
+
+
+    filename = input('What is the filename? (if empty, will find ''variablefile.mat'')','s');
+
+    if isempty(filename)
+
+        filename = 'variablefile.mat';
+
+    else
+
+        filename = [filename '.mat'];
+    
+    end
+
+    clearvars -except filename
+
+end
+
+load(filename);
+
+
+clc
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+symgroup = symvar(f);
+
+orif = symfun(f,symgroup);
+%%%%
+
+Realdata = Data;
+
+i=1;
+j=1;
+
+for Datanum = constantData
+    if ~isempty(cell2mat(Datanum))
+       Realdata(:,i) = cell2mat(Datanum);
+        
+    else
+        Realdata(:,i) = Data(:,j);
+        j=j+1;
+    end
+    
+        
+    i = i+1;
+end
+
+ArrayData = [];
+
+T = table;
+
+for i = 1:size(Realdata,2)
+    
+    %ArrayData{i} = Realdata(:,i)
+    T.(char(symgroup(i))) = Realdata(:,i);
+    
+end
+
+
+fprintf('\n\tOriginal Function\n')
+
+orif
+fprintf('\n\tVariables\n')
+symgroup
+
+fprintf('\n\tOriginal Data\n\n\n')
+disp(T)
+
+
+symfuncs = cell(0);
+
+i=1;
+
+for symbolic = symvar(f)
+   
+    symfuncs{i} = diff(f,symbolic);
+    
+    i=i+1;
+end
+
+Efuncs = symfun(cell2sym(symfuncs).^2,symgroup);
+
+Datacell = num2cell(Realdata);
+
+Datasum = [];
+
+
+    for i = 1: size(Datacell,1)
+    Datasum(i) = sqrt(sum((Efuncs(Datacell{i,:}).*(errorData.^2))));
+    Orical(i) = double(orif(Datacell{i,:}));
+    
+    end
+    
+    
+Datasum;
+double(Orical);
+
+
+
+T2 = table(Orical',Datasum','VariableNames',{'Calculation','Error'});
+fprintf('\n\n\t Calculated Data\n\n')
+disp(T2)
+
+OriginalData = T
+CalculationData = T2
+
+clearvars -except OriginalData CalculationData filename
